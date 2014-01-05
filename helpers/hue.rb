982e01d30df49c17c82719906cb31d8468817222
Ben Cox <benjamin.cox@jadedpixel.com>
2014-01-05 09:57:20 -0500

    Adding Hue on/off, bright/dim

--- helpers/hue.rb
+++
@@ -1,8 +0,0 @@
-@hue_client = Hue::Client.new
-
-def hue_io(state)
-  io = true unless state === "off"
-  hue_client.lights.each do |light|
-    light.on = io
-  end
-end
--- hubert.rb
+++ hubert.rb
@@ -2,12 +2,13 @@ require 'sinatra'
 require 'twilio-ruby'
 require 'hue'
 require_relative 'env.rb' #API Credentials
-require_relative 'helpers/hue.rb' #Hue action methods
 require_relative 'helpers/scheduler.rb' #the Scheduler 
 
-set :bind, '192.168.2.25'
+#set :bind, '192.168.2.25'
 
 helpers do 
+
+  #SMS Replies
   def reply(body)
     puts "Sending reply => #{body}."
 		twilio_client = Twilio::REST::Client.new ENV['twilio_sid'], ENV['twilio_token']
@@ -18,11 +19,13 @@ helpers do
     reply("Sorry, you need to tell me what to do with #{instruction}")
   end
 
+  #Core functions
   def nest(data)
 
   end
 
   def hue(data)
+    @hue_client = Hue::Client.new
     case data[1]
     when 'on'
       hue_io('on')
@@ -30,6 +33,12 @@ helpers do
     when 'off'
       hue_io('off')
       reply("I have turned off the lights")
+    when 'up'
+      hue_bri(255)
+      reply("I have turned the lights up")
+    when 'down'
+      hue_bri(128)
+      reply("I have turned the lights down")
     when 'status'
       #show status
       reply("The lights are something")
@@ -38,7 +47,23 @@ helpers do
       reply("Iunno, lights or something")
     end
   end
+
+  #Support functions
+  def hue_io(state)
+    io = true unless state === "off"
+    @hue_client.lights.each do |light|
+      light.on = io
+    end
+  end
+
+  def hue_bri(level)
+    @hue_client.lights.each do |light|
+      light.set_state({:bri => level})
+    end
+  end
 end
+
+#Routes
 
 get '/sms' do
 	puts "Received sms from #{params[:From]} that says #{params[:Body]}"
@@ -58,9 +83,4 @@ get '/sms' do
 	else
 		reply("Sorry, don't know what the means")
 	end
-end
-
-get '/log' do
-  content_type :txt
-  IO.popen('tail -f some.log')
 end
